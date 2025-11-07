25e83f9ec09c617749eb5fee390fef8403131726
estebanruster <emangascalva@byupathway.edu>
2025-11-07 17:12:55 -0600

    Navigation bar styled according to standard and best practices; color scheme used

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,6 +1,11 @@
 /*Handling the nav bar and menu button*/
 const hambutton = document.querySelector("#ham-button");
+const nav = document.querySelector("#nav-bar");
 
 hambutton.addEventListener('click', () => {
-    hambutton.classList.toggle('show');
+    hambutton.classList.toggle("show");
+    nav.classList.toggle("show")
 });
+
+/*Handling wayfinding in the menu*/
+let wayfinding = document.querySelector(".current");
