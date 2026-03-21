c8aaaa0447fb3e0bc3b005cb99439aaae2add173
estebanruster <emangascalva@byupathway.edu>
2026-03-21 13:01:14 -0600

    Cards in the member directory created. CSS contrast color pending to review. List button functioning; CSS styling pending.

--- chamber/scripts/directory-cards.js
+++ chamber/scripts/directory-cards.js
@@ -1,38 +1,110 @@
-const url = "../data/members.json";
-const cards = document.querySelector("#cards")
+const url = "./data/members.json";
+const cards = document.querySelector('#cards');
+let condition = true;
 
 async function getCompanieData() {
     const response = await fetch(url);
     const data = await response.json();
-    cardTemplate(data.companies);
+
+    if (condition) {
+        cardTemplate(data.companies);
+    } else {
+        listTemplate(data.companies);
+    }
+
+    //return data.companies;
 }
 
-const cardTemplate = (companies) => {
+function cardTemplate(companies) {
     companies.forEach((companie) => {
-        
+        let card = document.createElement('section');
+        let name = document.createElement('h2');
+        let business = document.createElement('p');
+        let membership = document.createElement('p');
+        let address = document.createElement('p');
+        let phone = document.createElement('p');
+        let website = document.createElement('a');
+        let logo = document.createElement('img');
+
+        name.textContent = `${companie.name}`;
+        business.textContent = `Business line: ${companie.businessLine}`;
+        if (companie.membershipLevel === 3) {
+            membership.textContent = `GOLD`;
+        }
+        if (companie.membershipLevel === 2) {
+            membership.textContent = `SILVER`;
+        }
+        else {
+            membership.textContent = `Member`;
+        }
+        address.textContent = `${companie.address}`;
+        phone.textContent = `Phone number: ${companie.phoneNumber}`;
+        website.setAttribute('href', `${companie.websiteUrl}`);
+        website.textContent = `${companie.websiteUrl}`;
+        logo.setAttribute('src', companie.image);
+        logo.setAttribute('alt', `Logo of ${companie.name}`);
+        logo.setAttribute('loading', 'lazy');
+        logo.setAttribute('width', '200');
+        logo.setAttribute('height', '200');
+
+        card.appendChild(name);
+        card.appendChild(business);
+        card.appendChild(membership);
+        card.appendChild(address);
+        card.appendChild(phone);
+        card.appendChild(website);
+        card.appendChild(logo);
+        cards.appendChild(card);
     });
 }
 
-getCompanieData();
+function listTemplate(companies) {
+    companies.forEach((companie) => {
+        let membership = "";
+        if (companie.membershipLevel === 3) {
+            membership = `GOLD`;
+        }
+        if (companie.membershipLevel === 2) {
+            membership = `SILVER`;
+        }
+        else {
+            membership = `Member`;
+        }
 
-function courseCard(courses) {
-    const html = courses.map(courseTemplate);
-    document.querySelector("#directory-cards").innerHTML = html.join("");
+        let list = document.createElement('ul');
+        let name = document.createElement('li');
+        let business = document.createElement('li');
+        let membershipLvl = document.createElement('li');
+        let address = document.createElement('li');
+        let phone = document.createElement('li');
+
+        name.textContent = `${companie.name}`;
+        business.textContent = `${companie.businessLine}`;
+        membershipLvl.textContent = `${membership}`;
+        address.textContent = `${companie.address}`;
+        phone.textContent = `${companie.phoneNumber}`;
+
+        list.appendChild(name);
+        list.appendChild(business);
+        list.appendChild(membershipLvl);
+        list.appendChild(address);
+        list.appendChild(phone);
+        cards.appendChild(list);
+    });
 }
 
-const allCourses = document.querySelector("#all");
-allCourses.addEventListener("click", () => {
-    courseCard(courses);
+const listView = document.querySelector("#list");
+listView.addEventListener("click", () => {
+    condition = false;
+    cards.innerHTML = '';
+    getCompanieData();
 });
 
-const wddCourses = document.querySelector("#wdd");
-wddCourses.addEventListener("click", () => {
-    courseCard(courses.filter(course => course.subject === "WDD"));
+const cardView = document.querySelector("#card");
+cardView.addEventListener("click", () => {
+    condition = true;
+    cards.innerHTML = '';
+    getCompanieData();
 });
 
-const cseCourses = document.querySelector("#cse");
-cseCourses.addEventListener("click", () => {
-    courseCard(courses.filter(course => course.subject === "CSE"));
-});
-
-courseCard(courses);
+getCompanieData();
