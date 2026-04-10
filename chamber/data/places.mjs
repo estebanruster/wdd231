a4e5d6902a348953b3e82b787b6ab970ae9ae18e
estebanruster <emangascalva@byupathway.edu>
2026-04-09 18:09:56 -0600

    JSON file in data folder created with info of 8 places to visit in Querétaro. Using modules to export the JSON data to a JS file that modifies the html and put the info of the JSON in the HTML.

---
+++ chamber/data/places.mjs
@@ -0,0 +1,58 @@
+export const places = [
+    {
+        "name": "Querétaro Aqueduct (Los Arcos)",
+        "description": "The city's most iconic 18th-century monument, featuring 74 pink stone arches stretching 1,280 meters.",
+        "address": "Calzada de los Arcos, Bosques del Acueducto, Santiago de Querétaro, Qro.",
+        "cost": "Free",
+        "image_url": "./images/discover-aqueduct-queretaro.webp"
+    },
+    {
+        "name": "Plaza de Armas (Historic Center)",
+        "description": "The social heart of the city, surrounded by colonial buildings like the Palacio de la Corregidora.",
+        "address": "C. 5 de Mayo s/n, Centro Histórico, Santiago de Querétaro, Qro.",
+        "cost": "Free",
+        "image_url": "./images/discover-downtown-queretaro.webp"
+    },
+    {
+        "name": "Temple and Ex-Convent of La Cruz",
+        "description": "A historical site home to the famous 'tree of thorns' in the shape of a cross and the end of the aqueduct.",
+        "address": "Ejército Republicano s/n, La Cruz, Santiago de Querétaro, Qro.",
+        "cost": "Temple: Free; Convent tour: approx. $20-$40 MXN",
+        "image_url": "./images/discover-cruz-queretaro.webp"
+    },
+    {
+        "name": "Querétaro Art Museum (San Agustín Cloister)",
+        "description": "Famous for its New Spanish Baroque courtyard, considered one of the most beautiful in Latin America.",
+        "address": "C. Ignacio Allende Sur 14, Centro, Santiago de Querétaro, Qro.",
+        "cost": "Approx. $30 MXN (Free on Sundays)",
+        "image_url": "./images/discover-museum-queretaro.webp"
+    },
+    {
+        "name": "Cerro de las Campanas (Hill of the Bells)",
+        "description": "A historical national park where Emperor Maximilian I was executed; includes a museum and memorial chapel.",
+        "address": "Cerro de las Campanas s/n, Centro Universitario, Santiago de Querétaro, Qro.",
+        "cost": "Park: $6 MXN; Museum: $17 MXN (approx.)",
+        "image_url": "./images/discover-hill-queretaro.webp"
+    },
+    {
+        "name": "Zenea Garden",
+        "description": "A traditional plaza where locals gather to dance danzón or listen to the state band on weekends.",
+        "address": "C. 16 de Septiembre 8-24, Centro, Santiago de Querétaro, Qro.",
+        "cost": "Free",
+        "image_url": "./images/discover-zenea-queretaro.webp"
+    },
+    {
+        "name": "Pantheon of Illustrious Queretanos",
+        "description": "A mausoleum and viewpoint where the remains of key historical figures rest.",
+        "address": "Ejército Republicano s/n, La Cruz, Santiago de Querétaro, Qro.",
+        "cost": "Free",
+        "image_url": "./images/discover-pantheon-queretaro.webp"
+    },
+    {
+        "name": "Pedestrian Walkways (Andador Matamoros / Madero)",
+        "description": "Lively streets filled with cafes, galleries, and craft shops connecting the main plazas.",
+        "address": "Andador Matamoros / Francisco I. Madero, Centro Histórico, Santiago de Querétaro, Qro.",
+        "cost": "Free",
+        "image_url": "./images/discover-walkways-queretaro.webp"
+    }
+]
---
+++ chamber/scripts/discover-cards.js
@@ -0,0 +1,52 @@
+//importing de JSON module data
+import { places } from "../data/places.mjs";
+//console.log(places);
+const discover = document.querySelector('#discover');
+
+function discoverCard(places) {
+    //forEach loop to create a div for each of the 8 objects in the array of the JSON
+    places.forEach(place => {
+        let card = document.createElement('div');
+        //creating all the child elements of the div
+        let title = document.createElement('h2');
+
+        //creating the figure tag and its child elements for the image
+        let figure = document.createElement('figure');
+        let photo = document.createElement('img');
+        let caption = document.createElement('figcaption');
+        //
+
+        let address = document.createElement('address');
+        let description = document.createElement('p');
+        let button = document.createElement('button');
+
+        //putting the JSON data in the created elements
+        title.textContent = `${place.name}`;
+        
+        photo.setAttribute('src', place.image_url);
+        photo.setAttribute('alt', `Photo of ${place.name}`);
+        photo.setAttribute('loading', 'lazy');
+        photo.setAttribute('width', '300');
+        photo.setAttribute('height', '200');
+        caption.textContent = `View of ${place.name}`;
+
+        address.textContent = `${place.address}`;
+        description.textContent = `${place.description}`;
+        button.setAttribute('type', 'button');
+        button.textContent = 'learn more';
+
+        //appending the child elements to make it part of the html document
+        figure.appendChild(photo);
+        figure.appendChild(caption);
+
+        card.appendChild(title);
+        card.appendChild(figure);
+        card.appendChild(address);
+        card.appendChild(description);
+        card.appendChild(button);
+
+        discover.appendChild(card);
+    });
+}
+
+discoverCard(places);
